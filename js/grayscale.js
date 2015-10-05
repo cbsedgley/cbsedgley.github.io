f7953e51cb36c26480eed5a89e9d010888d8237c
cbsedgley <cbsedgley@prox.local>
2015-10-04 22:46:02 -0400

    remove js

--- js/grayscale.js
+++ js/grayscale.js
@@ -24,11 +24,6 @@ $(function() {
     });
 });
 
-//On page scroll, scroll extra to show title
-var shiftWindow = function() { scrollBy(0, -50) };
-    if (location.hash) shiftWindow();
-        window.addEventListener("hashchange", shiftWindow);
-
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
